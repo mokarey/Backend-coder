7e7439b72435b3d4012fa0033c2536d550062f13
Valentin <107222626+mokarey@users.noreply.github.com>
2023-08-02 17:46:00 -0300

    HANDLEBARS FUNCIONANDO

--- src/articulosApp.js
+++ src/articulosApp.js
@@ -1,8 +1,22 @@
 import express from "express";
+import handlebars from "express-handlebars";
 import articulosRout from "./routes/articulosRout.js";
 import carritosRout from "./routes/carritosRout.js";
+import __dirname from "./dirname.js"
 
 const app = express();
+
+app.engine("handlebars", handlebars.engine());
+
+// seteamos el motor de vistas-- 
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    const { nombre } = req.query;
+    res.render("index", {nombre: nombre});
+})
+
 
 // transforma la informacion-- 
 app.use(express.json());
