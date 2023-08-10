b0144036a6ae71eff15c85be28d951d84c3174be
Valentin <107222626+mokarey@users.noreply.github.com>
2023-08-10 15:05:03 -0300

    ARREGLOS HANDLEBARS

--- src/articulosApp.js
+++ src/articulosApp.js
@@ -3,11 +3,11 @@ import handlebars from "express-handlebars";
 import articulosRout from "./routes/articulosRout.js";
 import articulosViewsRout from "./routes/articulosViewsRout.js";
 import carritosRout from "./routes/carritosRout.js";
-import __dirname from "./dirname.js"
+import {__dirname} from "./dirname.js";
 
 const app = express();
 
-app.engine("handlebars", handlebars.engine());
+app.engine("handlebars", handlebars());
 
 // seteamos el motor de vistas-- 
 app.set("views", `${__dirname}/views`);
